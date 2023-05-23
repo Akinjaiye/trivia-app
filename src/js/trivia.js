2e782d9c3070086b046d8547015b6a8395fb1a28
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-05-23 17:58:20 +0100

    add const levels in trivia.js

--- src/js/trivia.js
+++ src/js/trivia.js
@@ -1,8 +1,8 @@
  const game = document.querySelector('#game');
  const scoreDisplay = document.getElementById('score');
 
-// const film = 11
-// const levels = ['easy', 'medium', 'hard']
+const film = 11
+const levels = ['easy', 'medium', 'hard']
 
 //  function addGenre(){
 //     const column = document.createElement('div');
