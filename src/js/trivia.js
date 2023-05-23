084aa49707deaa9b47f9ca14a66cbbf251075090
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-05-23 18:59:21 +0100

    add fetch method in trivia.js

--- src/js/trivia.js
+++ src/js/trivia.js
@@ -6,5 +6,16 @@ const levels = ['easy', 'medium', 'hard']
 
  function addGenre(){
     const column = document.createElement('div');
-    column.classList.add('genre-column')
+    column.classList.add('genre-column');
+    column.innerHTML = 'this is a genre';
+    game.append(column);
+
+
+    levels.forEach(level => {
+        fetch(`https://opentdb.com/api.php?amount=1&category=11&difficulty=${level}&type=boolean`)
+         .then(response => response.json())
+         .then(data => console.log(data)) 
+    })
  }
+
+ addGenre();
