391aa1b0234acda3bdcd98db3368f99dcf30cb24
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-05-23 17:57:36 +0100

    add const game and scoredisplay

---
+++ src/js/trivia.js
@@ -0,0 +1,10 @@
+ const game = document.querySelector('#game');
+ const scoreDisplay = document.getElementById('score');
+
+// const film = 11
+// const levels = ['easy', 'medium', 'hard']
+
+//  function addGenre(){
+//     const column = document.createElement('div');
+//     column.classList.add('genre-column')
+//  }
