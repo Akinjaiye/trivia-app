2b83b1ec7b16600dc19d24aa7aa5e87d43ce3709
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-06-08 14:00:47 +0100

    add fnction addGenre in main.js

--- src/js/trivia.js
+++ src/js/trivia.js
@@ -1,5 +1,5 @@
 
-
+const game = document.getElementById('game');
 
 const genres = [
     {
@@ -20,11 +20,10 @@ const genres = [
     }
 ];
 
-const levels = ['easy', 'medium', 'hard']
+const levels = ['easy', 'medium', 'hard'];
 
-const game = document.getElementById('game')
-// const scoreDisplay = document.getElementById('score')
-// let score = 0
+
+
 
 
 export function addGenre(genre) {
@@ -61,53 +60,4 @@ export function addGenre(genre) {
 
     })
 }
-
-
-
-// export function flipCard() {
-//     this.innerHTML = ''
-//     this.style.fontSize = '15px'
-//     const textDisplay = document.createElement('div')
-//     const trueButton = document.createElement('button')
-//     const falseButton = document.createElement('button')
-//     trueButton.innerHTML = 'True'
-//     falseButton.innerHTML = 'False'
-//     trueButton.classList.add('true-button')
-//     falseButton.classList.add('false-button')
-//     trueButton.addEventListener('click', getResult)
-//     falseButton.addEventListener('click', getResult)
-//     textDisplay.innerHTML = this.getAttribute('data-question')
-//     this.append(textDisplay, trueButton, falseButton)
-
-//     const allCards = Array.from(document.querySelectorAll('.card'))
-//     allCards.forEach(card => card.removeEventListener('click', flipCard))
-// }
-
-// export function getResult() {
-//     const allCards = Array.from(document.querySelectorAll('.card'))
-//     allCards.forEach(card => card.addEventListener('click', flipCard))
-
-//     const cardOfButton = this.parentElement
-//     if (cardOfButton.getAttribute('data-answer') === this.innerHTML) {
-//         score = score + parseInt(cardOfButton.getAttribute('data-value'))
-//         scoreDisplay.innerHTML = score
-//         cardOfButton.classList.add('correct-answer')
-//         setTimeout(() => {
-//             while (cardOfButton.firstChild) {
-//                 cardOfButton.removeChild(cardOfButton.lastChild)
-//             }
-//             cardOfButton.innerHTML = cardOfButton.getAttribute('data-value')
-//         }, 100)
-//     } else {
-//         cardOfButton.classList.add('wrong-answer')
-//         setTimeout(() => {
-//             while (cardOfButton.firstChild) {
-//                 cardOfButton.removeChild(cardOfButton.lastChild)
-//             }
-//             cardOfButton.innerHTML = 0
-//         }, 100)
-//     }
-//     cardOfButton.removeEventListener('click',flipCard)
-// }
-
-// getResult();
+genres.forEach(genre => addGenre(genre));
--- src/main.js
+++ src/main.js
@@ -3,69 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import {addGenre} from './js/trivia.js'
 
-const game = document.getElementById('game')
-const scoreDisplay = document.getElementById('score')
-let score = 0
 
-const genres = [
-    {
-        name: 'Books',
-        id: 10
-    },
-    {
-        name: 'Film',
-        id: 11
-    },
-    {
-        name: 'Music',
-        id: 12
-    },
-    {
-        name: 'Video Games',
-        id: 15
-    }
-];
-
-
-
-//const levels = ['easy', 'medium', 'hard']
-
-// function addGenre(genre) {
-//     const column = document.createElement('div')
-//     column.classList.add('genre-column')
-//     column.innerHTML = genre.name
-//     game.append(column)
-
-//     levels.forEach(level => {
-//         const card = document.createElement('div')
-//         card.classList.add('card')
-//         column.append(card)
-
-//         if (level === 'easy') {
-//             card.innerHTML = 100
-//         }
-//         if (level === 'medium') {
-//             card.innerHTML = 200
-//         }
-//         if (level === 'hard') {
-//             card.innerHTML = 300
-//         }
-
-//         fetch(`https://opentdb.com/api.php?amount=1&category=${genre.id}&difficulty=${level}&type=boolean`)
-//             .then(response => response.json())
-//             .then(data => {
-               
-//                 // console.log(data)
-//                 card.setAttribute('data-question', data.results[0].question)
-//                 card.setAttribute('data-answer', data.results[0].correct_answer)
-//                 card.setAttribute('data-value', card.getInnerHTML())
-//             })
-//             .then(done => card.addEventListener('click', flipCard))
-
-//     })
-// }
-genres.forEach(genre => addGenre(genre));
-
+const scoreDisplay = document.getElementById('score');
+let score = 0;
 
 
 function flipCard() {
@@ -84,7 +24,7 @@ function flipCard() {
     this.append(textDisplay, trueButton, falseButton)
 
     const allCards = Array.from(document.querySelectorAll('.card'))
-    allCards.forEach(card => card.removeEventListener('click',       flipCard))
+    allCards.forEach(card => card.removeEventListener('click',flipCard))
 }
 
 function getResult() {
@@ -115,3 +55,5 @@ function getResult() {
 }
 
 getResult();
+
+addGenre();
